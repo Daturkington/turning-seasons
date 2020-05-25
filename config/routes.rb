72d54654d8d9ac7b6552c5350a1ac199b93607db
Daniel Turkington <turkington.ad@gmail.com>
2020-05-25 16:14:41 +0100

    routes set up for recipes and shopping list

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :ingredients, only: [:index, :show] do
-    resources :quantity
-    resources :shopping_list_items
-  end
   resources :recipes, only: [:index, :show]
+  resources :shoppinglist
 end
 
 #homepage
