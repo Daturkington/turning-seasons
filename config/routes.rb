5d7cb22b87f456d676ac0529f31981d54e094eb1
Daniel Turkington <turkington.ad@gmail.com>
2020-05-27 13:08:58 +0100

    Changes made to btn class in scss, added shoppint_list_items routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :recipes, only: [:index, :show]
+  resources :recipes, only: [:index, :show] do
+    resources :shopping_list_items
+  end
   resources :shopping_lists
 end
 
