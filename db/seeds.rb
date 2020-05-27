bb94e0015fdc47f465a8bdba12fefa595ee0f884
Alex Bottenberg <alexbottenberg@hotmail.com>
2020-05-27 18:45:13 +0100

    added to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,125 @@ Quantity.destroy_all
 Ingredient.destroy_all
 Recipe.destroy_all
 
+artichoke = Ingredient.create(name: "Artichoke", season: "june")
+oil = Ingredient.create(name: "Glug of extra-virgin olive oil")
+garlic = Ingredient.create(name: "Garlic glove")
+salt = Ingredient.create(name: "Pinch of sea salt")
+pepper = Ingredient.create(name: "Pinch of black pepper")
+wine = Ingredient.create(name: "White wine")
+burrata = Ingredient.create(name: "Burrata")
+flatbread = Ingredient.create(name: "Flatbread")
+pesto = Ingredient.create(name: "Pesto")
+mint = Ingredient.create(name: "Springs of mint")
+
+artichoke_recipe = Recipe.create(name:"Artichoke and burrata salad", author:"Tom Hunt", description: "Next time you make a recipe that calls for just the artichoke hearts, don’t discard the leaves – they can be frozen for later use or turned into a distinctive stock. Or, if you’d like to upcycle them into something a little more elaborate, this dish does just that. Burrata and artichokes are a match made in heaven, and the pesto seals the deal.", instructions: "Heat a thick-based frying pan on a medium heat with a good glug of extra-virgin olive oil. Add the artichoke leaves and the whole garlic clove, and season with sea salt. Fry gently for a few minutes, then add a slosh of white wine and cover.
+
+Leave to steam for five minutes, or until the leaves become tender; add more wine, if need be. Remove the lid and cook to evaporate any remaining wine. Douse the artichoke leaves with a little more olive oil and fry gently until caramelised.
+
+Serve as they are as a side vegetable, or turn into a centrepiece by serving on a platter with pieces of torn burrata, mint and crisp bread, and drizzled with pesto. Eat the leaves just as you would a boiled globe artichoke, pulling the flesh off with your teeth.")
+
+Quantity.create(measurement: 1, ingredient: artichoke, recipe: artichoke_recipe)
+Quantity.create(measurement: 1, ingredient: oil, recipe: artichoke_recipe)
+Quantity.create(measurement: 1, ingredient: garlic, recipe: artichoke_recipe)
+Quantity.create(measurement: 1, ingredient: salt, recipe: artichoke_recipe)
+Quantity.create(measurement: 1, ingredient: pepper, recipe: artichoke_recipe)
+Quantity.create(measurement: 1, ingredient: flatbread, recipe: artichoke_recipe)
+Quantity.create(measurement: 1, ingredient: pesto, recipe: artichoke_recipe)
+Quantity.create(measurement: 2, ingredient: mint, recipe: artichoke_recipe)
+
+asparagus = Ingredient.create(name: "Asparagus", season: "june")
+eggs = Ingredient.create(name: "Eggs")
+butter = Ingredient.create(name: "Butter")
+parsley = Ingredient.create(name: "Parsley")
+hazelnuts = Ingredient.create(name: "hazelnuts")
+sesame = Ingredient.create(name: "Sesame seeds")
+dried_chillies = Ingredient.create(name: "Dried red chillies")
+sugar = Ingredient.create(name: "Caster sugar")
+anchovies = Ingredient.create(name: "Anchovies")
+
+asparagus_recipe = Recipe.create(name:"Asparagus with fried egg and salsa macha", author:"Thomasina Miers", description: "Salsa macha is a Mexican sauce I am in love with. Garlic is poached gently in oil, followed by peanuts, dried red chillies and sesame seeds. The resulting (highly addictive) oil is drizzled over the hundreds of different seafood tostadas; a dish found up and down the coastline of Baja California.
+
+But I think it adds excitement to almost anything it touches, and goodness knows we need a bit of that while we’re all still cooped up at home. This is my hazelnut version, which I think is glorious with asparagus.", instructions: "Start with the salsa macha. Heat half the oil in a small pan over a very low heat, add the garlic cloves and simmer in the oil until golden – about five minutes. Remove with a slotted spoon into a bowl.
+
+Now fry the chillies until gently darkened in colour but not burnt – one to two minutes. Transfer to the bowl, and repeat with the hazelnuts, toasting until they, too, are a pale gold. Remove the nuts, roughly chopping half of them for the garnish, and put the rest in the bowl. Toast the sesame seeds in a dry frying pan.
+
+Advertisement
+Blitz the sesame seeds and chillies in an upright blender for one minute. Now add the whole hazelnuts, the garlic, sugar and anchovies (if you are using them) and pulse a few times. Add just enough oil to blitz to a rough paste. Add the rest of the oil, including the cooking oil, pulse once more, and taste, adding enough salt so that it tastes rounded and delicious.
+
+When you are ready to eat, heat two serving plates. Steam the asparagus for five minutes, then transfer to a griddle and cook over a very high heat until it is beautifully charred and tender.
+
+Meanwhile, melt the butter in the same frying pan you used to toast the sesame seeds, heating until it starts to turn deep brown. Transfer to a small bowl and pour in roughly the same amount of the salsa macha (be sure to stir before you add it). Keep this mixture warm.
+
+Wipe the pan, then fry the eggs in a little more butter until crisp around the edges. Serve the eggs on top of the grilled asparagus, pour over the sauce, then scatter with the parsley and chopped nuts. You will need lovely bread to mop up the sauce.")
+
+Quantity.create(measurement: 300, ingredient: asparagus, recipe: asparagus_recipe)
+Quantity.create(measurement: 4, ingredient: eggs, recipe: asparagus_recipe)
+Quantity.create(measurement: 60, ingredient: butter, recipe: asparagus_recipe)
+Quantity.create(measurement: 1, ingredient: parsley, recipe: asparagus_recipe)
+Quantity.create(measurement: 5, ingredient: garlic, recipe: asparagus_recipe)
+Quantity.create(measurement: 60, ingredient: hazelnuts, recipe: asparagus_recipe)
+Quantity.create(measurement: 40, ingredient: sesame, recipe: asparagus_recipe)
+Quantity.create(measurement: 10, ingredient: dried_chillies, recipe: asparagus_recipe)
+Quantity.create(measurement: 1, ingredient: sugar, recipe: asparagus_recipe)
+Quantity.create(measurement: 3, ingredient: anchovies, recipe: asparagus_recipe)
+Quantity.create(measurement: 300, ingredient: oil, recipe: asparagus_recipe)
+Quantity.create(measurement: 1, ingredient: salt, recipe: asparagus_recipe)
+
+
+aubergines = Ingredient.create(name: "Aubergine", season: "june")
+pistachios = Ingredient.create(name: "Pistachios")
+limes = Ingredient.create(name: "Limes")
+greek_yoghurt = Ingredient.create(name: "Greek yoghurt")
+sumac = Ingredient.create(name: "Sumac")
+
+aubergine_recipe = Recipe.create(name:"Aubergine wedges with lime and pistachio crumb", author:"Anna Jones", description: "Aubergine doesn’t get much attention in my kitchen; my husband, John, doesn’t like it (though I think this is because he has eaten so many badly cooked aubergines – not by me, of course). But a couple of weeks ago, I found myself pining for aubergine like it was a long-lost friend. I cooked up some fat wedges until they were dark and golden on the outside, topped them with a lime and pistachio crumb, and ate a happy solo lunch with flatbreads and salted yoghurt. I also tried it paired with tamarind in this sweet-and-sour curry, spooned on to sticky jasmine rice. My aubergine craving was quelled and, of course, John licked his plate clean.", instructions: "Cut the aubergines lengthways into six to eight wedges (see picture), depending on their size. In a large, nonstick frying pan, heat the oil and fry the aubergines in batches, turning carefully. Once each batch is golden all over, set aside on a roasting tray and keep warm in the oven while you fry the rest.
+
+Toast the pistachios, then chop and mix with the lime zest and a pinch of salt.
+
+Mix the lime yoghurt ingredients in a small bowl.
+
+Serve the aubergine wedges topped with the lime yoghurt and scattered with the chopped pistachios.")
+
+Quantity.create(measurement: 3, ingredient: aubergines, recipe: aubergine_recipe)
+Quantity.create(measurement: 3, ingredient: oil, recipe: aubergine_recipe)
+Quantity.create(measurement: 100, ingredient: pistachios, recipe: aubergine_recipe)
+Quantity.create(measurement: 2, ingredient: limes, recipe: aubergine_recipe)
+Quantity.create(measurement: 1, ingredient: salt, recipe: aubergine_recipe)
+Quantity.create(measurement: 250, ingredient: greek_yoghurt, recipe: aubergine_recipe)
+Quantity.create(measurement: 1, ingredient: sumac, recipe: aubergine_recipe)
+
+coconut_oil = Ingredient.create(name: "Coconut oil")
+shallots = Ingredient.create(name: "Shallots", season: "june")
+ginger = Ingredient.create(name: "Ginger")
+lemongrass = Ingredient.create(name: "Lemongrass")
+lime_leaves = Ingredient.create(name: "Makrut lime leave")
+red_chilli = Ingredient.create(name: "Red chillies", season: "july")
+ground_coriander = Ingredient.create(name: "Ground coriander")
+vine_tomatoes = Ingredient.create(name: "Vine tomatoes", season: "june")
+tamarind_paste = Ingredient.create(name: "Tamarind paste")
+coconut_milk = Ingredient.create(name: "Coconut milk")
+vegetable_stock = Ingredient.create(name: "Vegetable stock")
+thai_basil = Ingredient.create(name: "Thai basil")
+
+aubergine_recipe2 = Recipe.create(name:"Aubergine curry with basil and tamarind", author:"Anna Jones", description: "Use Thai basil if you can get it (although normal basil will do). If you can’t get lime leaves, add a good squeeze of lime into your curry at the end.", instructions: "Put a large pan over a medium heat, add a little oil, then fry the aubergine in batches until golden. Set aside.
+
+In the same pan, add a little more oil, if needed, followed by the shallots and ginger. Cook on a medium heat for around 10 minutes, until the shallots are soft and sweet. Add the garlic, lemongrass, lime leaves, chilli and ground coriander, and cook for a further five minutes. Add the chopped tomatoes, aubergine, tamarind, coconut milk and stock, then simmer for 25‑30 minutes, until the aubergines have almost collapsed and the sauce is thick, adding a little more water or stock if needed.
+
+Serve immediately with sticky rice, lots of Thai basil and fresh chilli.")
+
+Quantity.create(ingredient: coconut_oil, recipe: aubergine_recipe2)
+Quantity.create(measurement: 4, ingredient: aubergines, recipe: aubergine_recipe2)
+Quantity.create(measurement: 4, ingredient: shallots, recipe: aubergine_recipe2)
+Quantity.create(measurement: 5, ingredient: ginger, recipe: aubergine_recipe2)
+Quantity.create(measurement: 3, ingredient: garlic, recipe: aubergine_recipe2)
+Quantity.create(measurement: 1, ingredient: lemongrass, recipe: aubergine_recipe2)
+Quantity.create(measurement: 2, ingredient: lime_leaves, recipe: aubergine_recipe2)
+Quantity.create(measurement: 1, ingredient: red_chilli, recipe: aubergine_recipe2)
+Quantity.create(measurement: 1, ingredient: ground_coriander, recipe: aubergine_recipe2)
+Quantity.create(measurement: 300, ingredient: vine_tomatoes, recipe: aubergine_recipe2)
+Quantity.create(measurement: 2, ingredient: tamarind_paste, recipe: aubergine_recipe2)
+Quantity.create(measurement: 400, ingredient: coconut_milk, recipe: aubergine_recipe2)
+Quantity.create(measurement: 200, ingredient: vegetable_stock, recipe: aubergine_recipe2)
 
 ingredient1 = Ingredient.create(name: 'butter')
 p ingredient1.errors
