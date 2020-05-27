a92d65144252904c582f2b3500ec475a7f92beec
Daniel Turkington <turkington.ad@gmail.com>
2020-05-27 16:25:07 +0100

    Profile page displays ingredients, associates changed to access ingredients table from shopping list

--- app/models/shopping_list_item.rb
+++ app/models/shopping_list_item.rb
@@ -1,4 +1,6 @@
 class ShoppingListItem < ApplicationRecord
   belongs_to :recipe
   belongs_to :shopping_list
+  has_many :quantities, through: :recipe
+  has_many :ingredients, through: :quantities
 end
