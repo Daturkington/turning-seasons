9bc1a11bd08bc6d3d79ba1e24e45826d4a14b05c
Daniel Turkington <turkington.ad@gmail.com>
2020-05-28 17:10:57 +0100

    Delete method added for shopping list items

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :recipes, only: [:index, :show] do
-    resources :shopping_list_items
+    resources :shopping_list_items, except: [:destroy]
   end
+  resources :shopping_list_items, only: [:destroy]
   resources :shopping_lists
   resources :users, only: [:show]
 end
