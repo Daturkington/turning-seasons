1f72b14f74875ae8bef8c6353c96637a31a84ea3
Daniel Turkington <turkington.ad@gmail.com>
2020-05-29 12:13:21 +0100

    Added clkoudinary and included checks for user signed in

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,5 @@ class Recipe < ApplicationRecord
   has_many :ingredients, through: :quantities
   has_many :shopping_list_items, dependent: :destroy
   has_one :favourite
+  has_one_attached :photo
 end
